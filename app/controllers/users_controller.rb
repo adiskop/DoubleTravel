09ca58969edf3b7e8cf49555334ab2393401d47a
Adi Skop <adiskop@gmail.com>
2021-05-16 12:28:29 +0200

    set up controllers for signing up a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            # log user in 
+            session[:user_id] = @user.id
+            # redirect user to his show page
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+#in order to tell our params that some things are premitted, but some aren't:
+
+private
+
+def user_params
+    params.require(:user).permit(:name, :email, :password)
+end
+
 end
