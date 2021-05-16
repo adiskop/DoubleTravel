886593b6558aded7299299a1c8b737be94371332
Adi Skop <adiskop@gmail.com>
2021-05-16 12:29:58 +0200

    set migrations

--- db/migrate/20210515092058_create_destinations.rb
+++ db/migrate/20210515092058_create_destinations.rb
@@ -3,7 +3,7 @@ class CreateDestinations < ActiveRecord::Migration[6.1]
     create_table :destinations do |t|
       t.string :name
       t.string :continent
-      t.belongs_to :user, null: false, foreign_key: true
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20210515100104_create_visited_gems.rb
+++ db/migrate/20210515100104_create_visited_gems.rb
@@ -3,6 +3,8 @@ class CreateVisitedGems < ActiveRecord::Migration[6.1]
     create_table :visited_gems do |t|
       t.string :name
       t.text :description
+      t.integer :user_id
+      t.integer :destination_id
 
       t.timestamps
     end
