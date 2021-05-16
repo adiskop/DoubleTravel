33f4b31436fa78d42605912947a425ed3243b832
Adi Skop <adiskop@gmail.com>
2021-05-16 12:29:38 +0200

    set a secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :destinations
     has_many :visited_gems
     has_many :destinations, through: :visited_gems
+
+    has_secure_password #give us access to authenticate method, validate password
 end
